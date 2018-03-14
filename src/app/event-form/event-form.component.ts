a26399d868dd35cf8de88eb932ffa474cebedda3
Catriona Crowe <ccrowe201@caledonian.ac.uk>
2018-03-14 18:13:21 +0000

    added the event component

---
+++ src/app/event-form/event-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Event }    from '../event.model';
+
+@Component({
+  selector: 'app-event-form',
+  templateUrl: './event-form.component.html',
+  styleUrls: ['./event-form.component.css']
+})
+export class EventFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
