dac08d31ca4c5897e2182164d179b980c22116f7
Chris Connor <cjconnor24@connordesign.com>
2018-03-22 11:49:13 +0000

    Tweaking routes for testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { TimelineListComponent } from './timeline-list/timeline-list.component';
 import { RegisterComponent } from './register/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: './register', pathMatch: 'full' },
-  { path : './register', component: RegisterComponent},
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path : 'register', component: RegisterComponent},
   { path : './dashboard', component: DashboardComponent},
   { path : './timeline-list', component: TimelineListComponent}
 ];
