d5f0cee3ed605dc64097b9d6719179e30e2c1d3c
Chris Connor <cjconnor24@connordesign.com>
2018-03-22 11:55:33 +0000

    Somewhere to store timeline events

--- src/app/models/timeline.model.ts
+++ src/app/models/timeline.model.ts
@@ -2,10 +2,12 @@ export class Timeline {
 
     timelineId: string;
     title: string;
+    events: Event[];
 
     constructor(title:string){
         this.title = Math.floor((Math.random()*6)+1).toString();
         this.title = title;
+        this.events = [];
     }
 
 }
