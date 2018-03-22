ffb10d47a38ad97978ba367e066054d391bb94a5
Chris Connor <cjconnor24@connordesign.com>
2018-03-22 11:54:21 +0000

    Adding basic constructor

--- src/app/models/timeline.model.ts
+++ src/app/models/timeline.model.ts
@@ -3,4 +3,9 @@ export class Timeline {
     timelineId: string;
     title: string;
 
+    constructor(title:string){
+        this.title = Math.floor((Math.random()*6)+1).toString();
+        this.title = title;
+    }
+
 }
