469768bdd03f5644c025218a0f7edc5c3784bf17
Chris Connor <cjconnor24@connordesign.com>
2018-03-24 21:05:26 +0000

    Adding more routes

--- src/app/routing-module/app-routing.module.ts
+++ src/app/routing-module/app-routing.module.ts
@@ -5,12 +5,13 @@ import { RegisterComponent } from '../register/register/register.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { TimelineListComponent } from '../timeline-list/timeline-list.component';
 import { CreateTimelineComponent } from '../register/create-timeline/create-timeline.component';
+import { RegisterListComponent } from '../register/register-list/register-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'timelines', pathMatch: 'full' },
+  // { path: '', redirectTo: 'timelines', pathMatch: 'full' },
   {
     path: 'timelines', component: RegisterComponent, children: [
-      // { path: '', component: RegisterComponent },   //NEEDS UPDATED
+      { path: '', component: RegisterListComponent } ,   //NEEDS UPDATED
       { path: 'new', component: CreateTimelineComponent }, //NEEDS UPDATED
       { path: ':id', component: RegisterComponent }, //NEEDS UPDATED
       { path: ':id/edit', component: RegisterComponent } //NEEDS UPDATED
