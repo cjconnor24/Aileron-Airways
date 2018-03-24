ee17f9d7d1ddc4a28bbd42373bf28859deb6c5d3
Chris Connor <cjconnor24@connordesign.com>
2018-03-24 15:29:38 +0000

    Adding child routes for RESTful pattern

--- src/app/routing-module/app-routing.module.ts
+++ src/app/routing-module/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Routes, RouterModule} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from '../register/register/register.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { TimelineListComponent } from '../timeline-list/timeline-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'register', pathMatch: 'full' },
-  { path : 'register', component: RegisterComponent},
-  { path : './dashboard', component: DashboardComponent},
-  { path : './timeline-list', component: TimelineListComponent}
+  { path: '', redirectTo: 'timelines', pathMatch: 'full' },
+  {
+    path: 'timelines', component: RegisterComponent, children: [
+      { path: '', component: RegisterComponent },   //NEEDS UPDATED
+      { path: 'new', component: RegisterComponent }, //NEEDS UPDATED
+      { path: ':id', component: RegisterComponent }, //NEEDS UPDATED
+      { path: ':id/edit', component: RegisterComponent } //NEEDS UPDATED
+    ]
+  },
+  // { path : './dashboard', component: DashboardComponent},
+  // { path : './timeline-list', component: TimelineListComponent}
 ];
 
 
@@ -18,7 +25,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes)
   ],
 
-  exports:[
+  exports: [
     RouterModule
   ]
 
