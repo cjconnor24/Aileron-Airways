2e39a69d11388c603a665952055fa23425137bf8
Chris Connor <cjconnor24@connordesign.com>
2018-03-27 21:11:04 +0100

    Done HUGE amount of work on the services
    Refactored so that ideagen sets up the observables but the register service subscribes and updates

--- src/app/services/ideagen.service.ts
+++ src/app/services/ideagen.service.ts
@@ -19,11 +19,11 @@ import { Subscription } from 'rxjs/Subscription';
 @Injectable()
 export class IdeagenService {
 
-  constructor(private httpClient: HttpClient, private registerService: RegisterService) { }
+  constructor(private httpClient: HttpClient) { }
 
   API_URL = environment.apiUrl;
 
-  createTimeline(timeline: Timeline) {
+  createTimeline(timeline: Timeline): Observable<Timeline> {
 
     const headers = new HttpHeaders(
       {
@@ -37,20 +37,20 @@ export class IdeagenService {
     const body = {
       'TenantId': 'Team2',
       'AuthToken': 'b3872e1b-12e3-4852-aaf0-a3d87d597282',
-      TimelineId: 'timelineid', // NEEDS AN ID
+      TimelineId: timeline.timelineId, // NEEDS AN ID
       Title: timeline.title
     };
-
-    console.log(body);
 
     return this.httpClient.put(this.API_URL + 'Timeline/Create', body,
       {
         headers: headers
-      }).subscribe(
-        (data: any) => {
-          console.log(data);
-        }
-      );
+      }).map(
+        (data: {Title: string, CreationTimeStamp: string, IsDeleted: boolean, Id: string, TenantId: string}) => {
+        const tline: Timeline =  new Timeline(data.Title);
+        tline.dateCreated = new Date(); //TODO: CONVERT TO PROPER TIME STAMP
+        return tline;
+        // return data;
+      });
 
   }
 
@@ -63,40 +63,26 @@ export class IdeagenService {
       }
     );
 
-    console.log(timeline);
-
     const body = {
       'TenantId': 'Team2',
       'AuthToken': 'b3872e1b-12e3-4852-aaf0-a3d87d597282',
       TimelineId: timeline.timelineId
-    }
-
-    console.log(body);
+    };
 
     return this.httpClient.put(this.API_URL + 'Timeline/Delete', body,
       {
         headers: headers
-      }).subscribe(
-        (data: any) => {
-          console.log(data);
-
-          this.registerService.deleteTimeline(timeline);
-
-
-        }
-      );
+      });
 
   }
 
 
-  getTimelines() {
+  getTimelines(): Observable<Timeline[]> {
 
-    const headers = new HttpHeaders(
-      {
-        'TenantId': 'Team2',
-        'AuthToken': 'b3872e1b-12e3-4852-aaf0-a3d87d597282'
-      }
-    );
+    const headers = new HttpHeaders({
+      'TenantId': 'Team2',
+      'AuthToken': 'b3872e1b-12e3-4852-aaf0-a3d87d597282'
+    });
 
     return this.httpClient.get(this.API_URL + 'Timeline/GetAllTimelinesAndEvent',
       {
@@ -104,6 +90,7 @@ export class IdeagenService {
       }).map(
         data => {
           return data['Timelines'].map(timeline => {
+
             const tl = new Timeline(timeline.Title);
             tl.timelineId = timeline.Id;
             tl.dateCreated = new Date((timeline.CreationTimeStamp - 621355968000000000) / 10000);
@@ -116,17 +103,6 @@ export class IdeagenService {
 
             return tl;
           });
-        }
-      )
-      .subscribe(
-        (timelines: Timeline[]) => {
-
-          console.log('Pulling data getTimelines() Ideagen Service:');
-          console.log(timelines);
-
-
-          this.registerService.setTimelines(timelines);
-
         }
       );
 
--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -3,69 +3,109 @@ import { Timeline } from '../models/timeline.model';
 import { Event } from '../models/event.model';
 import { Subject } from 'rxjs/Subject';
 import { IdeagenService } from './ideagen.service';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class RegisterService {
 
-  constructor() { }
+  constructor(private ideagenService: IdeagenService) { }
 
-  register: Timeline[] = [
-    new Timeline('Chris Event'),
-    new Timeline('Hardcoded Event')
-  ];
-
-  registerEv: Event[];
-
+  // register: Timeline[] = [
+  //   new Timeline('Chris Event'),
+  //   new Timeline('Hardcoded Event')
+  // ];
+  // register: Observable<Timeline[]>;
+  // registerEv: Event[];
+  
+  register: Timeline[] = [new Timeline('Chris Event')];
   registerChanged = new Subject<Timeline[]>();
-  registerChangeEv = new Subject<Event[]>();
 
-  getTimelines() {
+  // oTimelines: Observable<Timeline[]>;
+  // registerChangeEv = new Subject<Event[]>();
+
+  private loadTimelines() {
+
+    this.ideagenService.getTimelines()
+    .subscribe(
+      (timelines: Timeline[]) => {
+        this.register = timelines;
+        this.registerChanged.next(this.register.slice());
+
+        console.log('LOADING TIMELINES FROM API:');
+      },
+      (error: any) => {
+        console.log(error);
+      }
+);
+
+  }
+
+  getTimelines(): Timeline[] {
+    this.loadTimelines();
     return this.register.slice();
   }
 
-  getTimeline(id: string): Timeline  {
+  getTimeline(id: string): Timeline {
     return this.register.find(timeline => timeline.timelineId === id);
   }
 
   deleteTimeline(timeline: Timeline) {
     // TODO: DELETE TIMELINE
-    // this.register;
+    
+    this.ideagenService.deleteTimeline(timeline).subscribe(
+      (data: any) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log('There was an error with delete timeline:' + error);
+      }
+    );
+
     this.register.splice(this.register.indexOf(timeline), 1);
     this.registerChanged.next(this.register.slice());
 
 
   }
 
-  getAllEvent(){
-    return this.registerEv.slice();
-  }
+  // getAllEvent() {
+  //   return this.registerEv.slice();
+  // }
 
   setTimelines(timelines: Timeline[]) {
     this.register = timelines;
     this.registerChanged.next(this.register.slice());
   }
 
-  setEvent(events: Event[]) {
-    this.registerEv = events;
-    this.registerChangeEv.next(this.registerEv.slice());
-  }
+  // setEvent(events: Event[]) {
+  //   this.registerEv = events;
+  //   this.registerChangeEv.next(this.registerEv.slice());
+  // }
 
-  addEvent(event: Event){
-    this.registerEv.push(event);
-    this.registerChangeEv.next(this.registerEv.slice());
-  }
+  // addEvent(event: Event) {
+  //   this.registerEv.push(event);
+  //   this.registerChangeEv.next(this.registerEv.slice());
+  // }
 
   addTimeline(timeline: Timeline) {
+
+    this.ideagenService.createTimeline(timeline).subscribe(
+      (tline: Timeline) => {
+        // console.log(data);
+        this.register.push(tline);
+        this.registerChanged.next(this.register.slice());
+      },
+      (error) => {
+        console.log('There was an error creating timeline: ' + error);
+      }
+    );
+
     this.register.push(timeline);
     this.registerChanged.next(this.register.slice());
   }
 
-  updateTimeline(index: number, timeline: Timeline) {
-    
-    // TODO: UPDATE TIMELINE
-
-
-  }
+  // updateTimeline(index: number, timeline: Timeline) {
+  //   // TODO: UPDATE TIMELINE
+  // }
 
 
 
