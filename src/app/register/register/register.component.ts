0093c538b06d643ce354b7da14e3ffa645ec05a5
Chris Connor <cjconnor24@connordesign.com>
2018-04-01 14:55:25 +0100

    Trying to load the data on the register component

--- src/app/register/register/register.component.ts
+++ src/app/register/register/register.component.ts
@@ -1,13 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IdeagenService } from '../../services/ideagen.service';
+import { RegisterService } from '../../services/register.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Timeline } from '../../models/timeline.model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, OnDestroy {
 
+  register: Timeline[];
+  subscriber: Subscription;
+  // searchText: string;
+  // loaded = false;
 
+  constructor(private registerService: RegisterService) { }
+
+  ngOnInit() {
+    // GET THE STATIC TIMELINES FROM THE SERVICE THEN LISTEN TO THE SUBJECT
+    this.register = this.registerService.getTimelines();
+    this.subscriber = this.registerService.registerChanged.subscribe(
+      (timelines: Timeline[]) => {
+        this.register = timelines;
+        // this.loaded = true;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriber.unsubscribe();
+    console.log('Register List Subscription Destroyed');
+  }
 
 }
