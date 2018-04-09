bd7c93af83d7ef79dc47f8724182c9f760c35485
Matt Hedderly <mhedde200@caledonian.ac.uk>
2018-04-09 14:46:56 +0100

    added test code for doughnut chart, not functional yet

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from '../data.service';
+import { Chart } from 'chart.js';
 
 
 @Component({
@@ -12,6 +13,28 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var data;
+
+    data = {
+      datasets: [{
+          data: [10, 20, 30]
+      }],
+  
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: [
+          'Red',
+          'Yellow',
+          'Blue'
+      ]
+  };
+
+    var myDoughnutChart = new Chart('test', {
+      type: 'doughnut',
+      data: data,
+      options: {}
+  });
+  
+  }
   }
 
 }
