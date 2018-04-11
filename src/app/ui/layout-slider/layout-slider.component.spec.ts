46bf11b6d4910e3315031a7fe14eec2cabcbc7a9
Chris Connor <cjconnor24@connordesign.com>
2018-04-11 23:06:38 +0100

    Creating and adding layout slider component

---
+++ src/app/ui/layout-slider/layout-slider.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LayoutSliderComponent } from './layout-slider.component';
+
+describe('LayoutSliderComponent', () => {
+  let component: LayoutSliderComponent;
+  let fixture: ComponentFixture<LayoutSliderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LayoutSliderComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LayoutSliderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
