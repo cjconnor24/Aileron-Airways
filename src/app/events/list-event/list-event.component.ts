f02565e69d29106772e644c2eb9fa858844cf279
Chris Connor <cjconnor24@connordesign.com>
2018-04-11 23:49:23 +0100

    Creating local templates for when timline has no events

--- src/app/events/list-event/list-event.component.ts
+++ src/app/events/list-event/list-event.component.ts
@@ -15,6 +15,7 @@ export class ListEventComponent implements OnInit {
   id: string; 
   timeline: Timeline;
   loaded: boolean = false;
+  hasEvents: boolean;
 
   constructor(
     private registerService: RegisterService,
@@ -25,24 +26,23 @@ export class ListEventComponent implements OnInit {
 
   ngOnInit() {
 
-    console.log('LIST EVENT COMPONENT');
-
+    // GET THE ID FROM THE ROUTE
     this.route.params.subscribe(
       (params: Params) => {
 
         this.id = params['id'];
-        console.log('EVENT IS');
-        console.log(this.id);
         // this.editMode = params['id'] != null;
         // this.timeline = this.registerService.getTimeline(this.id);
         // console.log(this.timeline);
         // console.log(this.editMode);
         // this.initForm();
 
+        // GET THE TIMELINE AND EVENTS FROM THE API
         this.ideagenService.getTimelineAndEvents(this.id)
         .subscribe((data:Timeline)=>{
           this.timeline = data;
           this.loaded = true;
+          this.hasEvents = (data.events.length>0);
           console.log(data);
           
         })
