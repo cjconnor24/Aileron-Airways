622f2eeadea9aeeaa5f612b0ac790a2b78b4ca14
Chris Connor <cjconnor24@connordesign.com>
2018-04-15 17:39:25 +0100

    Some tweaks on dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,7 +11,7 @@ import { repeat } from 'rxjs/operators';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
@@ -68,7 +68,7 @@ export class DashboardComponent implements OnInit {
     },
     options:{
       title:{
-        text:"Timelines",
+        // text:"Timelines",
         display: true
       },
       cutoutPercentage: 70,
